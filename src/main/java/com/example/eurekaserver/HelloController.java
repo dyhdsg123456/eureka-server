663beebd807e0d40af7e96eab366e30191ac0ede
duyuanhao <13775516233@163.com>
2019-05-16 10:43:24 +0800

    1

---
+++ src/main/java/com/example/eurekaserver/HelloController.java
@@ -0,0 +1,34 @@
+package com.example.eurekaserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: dyh
+ * @Date: 2018/5/10
+ * @Description:
+ */
+@RestController
+public class HelloController {
+    @Value("${abc}")
+    private int a;
+
+    
+    @Autowired
+    private CounterService counterService;
+
+
+    @RequestMapping("hello")
+    public String index(){
+        System.out.println(a);
+        return String.valueOf(a);
+    }
+    @RequestMapping("hello2")
+    public String greet(){
+        counterService.increment("didispace.hello.count");
+        return "";
+    }
+}
